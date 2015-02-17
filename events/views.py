2c7ac51180e2b2a3cf5204c13209296df4a18e16
samynv <d.diks@mail.blastedsolutions.com>
2015-02-18 00:12:17 +0100

    Added POST handling of new events

--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 import forms
 
@@ -28,3 +28,15 @@ class NewEventView(View):
         context['event_form'] = forms.EventForm()
 
         return render(request, 'events/createEvent.html', context)
+
+    def post(self, request):
+        context = {}
+
+        form = forms.EventForm(request.POST)
+        if form.is_valid():
+             form.clean()
+             form.save()
+             return redirect("event:event")
+        else:
+            context["event_form"] = form
+            return render(request, 'events/createEvent.html', context)
