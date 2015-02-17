3fd273840c03d836776561d365d5b0b446439fd1
samynv <d.diks@mail.blastedsolutions.com>
2015-02-17 23:45:21 +0100

    Added Attendees per event

--- events/models.py
+++ events/models.py
@@ -13,11 +13,23 @@ class Event(models.Model):
     def __str__(self):
         return self.name
 
+    def GetAllAttendees(self):
+        return Registration.objects.filter(related_event=self)
+
+    def GetAllConfirmedAttendees(self):
+        return self.GetAllAttendees().filter(paid=True)
+
+    def GetAllPendingAttendees(self):
+        return self.GetAllAttendees().filter(paid=False)
+
 class Registration(models.Model):
     related_event = models.ForeignKey(Event)
     related_user = models.ForeignKey(User)
 
     date_created = models.DateTimeField(auto_now_add=True, name="date_registered", verbose_name="Date Registered")
+
+    paid = models.BooleanField(default=False)
+    date_paid = models.DateTimeField(null=True)
 
 class Access(models.Model):
     related_registration = models.ForeignKey(Registration)
