e4aadb65fd08ba5501fab997d3ddb0f9cd23c266
samynv <d.diks@mail.blastedsolutions.com>
2015-02-17 23:10:25 +0100

    Added basic models for events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.TextField()
+    description = models.TextField()
+
+    registration_open = models.BooleanField(default=False)
+    event_date = models.DateTimeField()
+
+class Registration(models.Model):
+    related_event = models.ForeignKey(Event)
+    related_user = models.ForeignKey(User)
+
+    date_created = models.DateTimeField(auto_now_add=True, name="date_registered", verbose_name="Date Registered")
+
+class Access(models.Model):
+    related_registration = models.ForeignKey(Registration)
+
+    date_created = models.DateTimeField(auto_now_add=True, name="date_entered", verbose_name="Date Entered")
+
+class Payment(models.Model):
+    related_registration = models.ForeignKey(Registration)
+
+    date_created = models.DateTimeField(auto_now_add=True, name="data_paid", verbose_name="Date Paid")
+
+
