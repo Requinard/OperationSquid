def9470378b346aacc703147280076ba38ad4e53
samynv <d.diks@mail.blastedsolutions.com>
2015-02-17 23:11:57 +0100

    Added basic admin interface

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from models import *
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Registration)
+admin.site.register(Access)
+admin.site.register(Payment)
